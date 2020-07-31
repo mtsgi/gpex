c67933e9cca48980fed887565dcbbcdb00470e29
mtsgi <oq@live.jp>
2020-08-01 05:41:29 +0900

    Add entire documents downloader

--- src/content/content.js
+++ src/content/content.js
@@ -1,8 +1,51 @@
 global.browser = require('webextension-polyfill');
 
 global.browser.runtime.onMessage.addListener(async request => {
-  console.log(request);
-  return { response: 'ok' };
+  const result = [];
+  const selector = 'a[aria-label][data-focus-id][target=_blank]';
+  const docsTypes = ['Google ドキュメント', 'Google スプレッドシート', 'Google スライド'];
+  switch (request.query) {
+    case 'getDocs':
+      document.querySelectorAll(selector).forEach(doc => {
+        if (doc.title) {
+          let type = doc.querySelector('div > div > div') ? doc.querySelector('div > div > div').textContent : '不明';
+          let downloadable = true;
+          let gdocs = false;
+          let defaultExportType = null;
+          let dlLinkDir = null;
+          if (type.indexOf('YouTube') === 0) {
+            type = 'YouTube';
+            downloadable = false;
+          }
+          if (docsTypes.includes(type)) {
+            gdocs = true;
+            if (type === 'Google ドキュメント') {
+              defaultExportType = 'docx';
+              dlLinkDir = 'document';
+            } else if (type === 'Google スプレッドシート') {
+              defaultExportType = 'xlsx';
+              dlLinkDir = 'spreadsheets';
+            } else if (type === 'Google スライド') {
+              defaultExportType = 'pdf';
+              downloadable = false;
+            }
+          }
+          result.push({
+            visibility: downloadable ? Boolean(doc.offsetHeight) : false,
+            title: doc.title,
+            href: doc.getAttribute('href'),
+            type,
+            downloadable,
+            gdocs,
+            defaultExportType,
+            dlLinkDir
+          });
+        }
+      });
+      return result;
+    default:
+      return false;
+  }
 });
 
 // Google Forms
